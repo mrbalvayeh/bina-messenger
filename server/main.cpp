969d3a43500c55c829febc41faf4cfc6b4106c13
mrbalvayeh <mrbalvayeh@hotmail.com>
2025-03-29 02:54:24 +0330

    Ensured Zmq init

--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,20 @@
-//
-// Created by dev on 3/26/25.
-//
+#include <QCoreApplication>
+#include "DatabaseManager.h"
+#include "ZmqServer.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    DatabaseManager dbManager;
+    if (!dbManager.initializeDB()) {
+        qWarning() << "Failed to initialize PostgreSQL. Exiting.";
+        return -1;
+    }
+
+    // Start ZeroMQ server
+    ZmqServer zmqServer(dbManager);
+    zmqServer.run("tcp://*:5555"); // Blocks forever
+
+    return app.exec(); // Actually, with the while(true) in run(), you might never get here
+}
